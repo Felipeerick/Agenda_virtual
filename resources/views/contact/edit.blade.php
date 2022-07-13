f99c2aa8d05a8ef422da6935131fe03a01177623
Felipe Erick <rick0531927@gmail.com>
2022-07-13 09:38:40 -0300

    ajeitando o edit e colocando um botão de listagem de contatos no layout

--- resources/views/contact/edit.blade.php
+++ resources/views/contact/edit.blade.php
@@ -10,38 +10,38 @@
  <div class='mb-3'>
 
 <label for="name" class="form-label">Nome</label>
-<input type="text"  class="form-control"id='name' name='name'>
+<input type="text"  class="form-control"id='name' value='{{ $contacts->name }}' name='name'>
 
 <label for="email" class="form-label">Email</label>
-<input type="text" class="form-control" id='email' name='email'>
+<input type="text" class="form-control" id='email' value='{{ $contacts->email }}'  name='email'>
 
 <label for="Senha" class="form-label">Senha</label>
-<input type="password" class="form-control" id='Senha' name='senha'>
+<input type="password" class="form-control" id='Senha'value='{{ $contacts->password }}' name='password'>
 
 <label for="CPF" class="form-label">CPF</label>
-<input type="text" class="form-control" id='CPF' name='cpf'>
+<input type="text" class="form-control" id='CPF'value='{{ $contacts->cpf }}' name='cpf'>
 
 <label for="tel" class="form-label">Número de telefone</label>
-<input type="text" class="form-control" id='tel' name='tel'>
+<input type="text" class="form-control" id='tel' value='{{ $contacts->tel }}' name='tel'>
 
 <label for="foto" class="form-label">Foto</label>
-<input type="file"  class="form-control"id='foto' name='photo'>
+<input type="file"  class="form-control"id='foto' value='{{ $contacts->photo }}' name='photo'>
  
      <hr>
 
   <h2>Endereço</h2>   
 <label for="Cep" class="form-label">Cep</label>
-<input type="text"  class="form-control"id='Cep' name='Cep'>
+<input type="text"  class="form-control"id='Cep' value='{{ $contacts->cep }}' name='cep'>
 
 <label for="rua" class="form-label">Rua</label>
-<input type="rua"  class="form-control"id='rua' name='street'>
+<input type="rua"  class="form-control"id='rua' value='{{ $contacts->street }}' name='street'>
 
 
 <label for="Bairro" class="form-label">Bairro</label>
-<input type="text" class="form-control" id='Bairro' name='neighborhood'>
+<input type="text" class="form-control" id='Bairro' value='{{ $contacts->neighborhood }}' name='neighborhood'>
 
 <label for="Estado" class="form-label">Estado</label>
-<input type="text"  class="form-control"id='Estado' name='state'>
+<input type="text"  class="form-control"id='Estado' value='{{ $contacts->state }}' name='state'>
 
 </div>
 
--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -15,11 +15,11 @@
             
             <li class="d-flex sm-3 naveg">
             <a href="#" class="tag btn btn-outline-light ">{{Auth::user()->name}}</a>
-            
-
+            <a href="{{ route('contacts.index') }}" class="tag btn btn-outline-light ">Contatos</a>
+             
             <form method="POST" action="{{ route('logout') }}">
                                             @csrf
-                                          <button type="submit" class="btn btn-outline-light" >Sair</button>                         
+                                          <button type="submit" class="tag btn btn-outline-light" >Sair</button>                         
                                         </form>
             </li>
 
