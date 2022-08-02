42492f1e95b4bc0bcabe8fdd43ce16a4af0734ff
Felipe Erick <rick0531927@gmail.com>
2022-08-02 16:30:53 -0300

    corrigindo o AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') === 'true') {
+        if (config('app.env') === 'production') {
             URL::forceScheme('https');
         }
     }
